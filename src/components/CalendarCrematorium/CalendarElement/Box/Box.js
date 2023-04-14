8c606483cc90ffa9e6dc9a4952bb35dfc112679f
Mateusz Sawicki <mateusz.saw00@wp.pl>
2023-04-14 03:49:39 +0200

    f

--- src/components/CalendarCrematorium/CalendarElement/Box/Box.js
+++ src/components/CalendarCrematorium/CalendarElement/Box/Box.js
@@ -1,18 +1,257 @@
 import styles from './Box.module.scss';
+import React, { useState } from 'react';
 
 function Box(props) {
 
   const closeWindow = () => {
     if (props.windowVisibility === true) {
       props.setWindowVisibility(false);
-      console.log(props.windowVisibility);
+      props.setEvent('');
     }
   };
 
+  // name
+
+  const [name, setName] = useState('');
+
+  const handleNameInputChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+  };
+
+  // surname
+
+  const [surname, setSurname] = useState('');
+
+  const handleSurnameInputChange = (event) => {
+    const value = event.target.value;
+    setSurname(value);
+  };
+
+  // weight
+
+  const [weight, setWeight] = useState('');
+
+  const handleWeightInputChange = (event) => {
+    const value = event.target.value;
+    setWeight(value);
+  };
+
+  // family
+
+  const [yesChecked, setYesChecked] = useState(false);
+  const [noChecked, setNoChecked] = useState(false);
+
+  const handleYesChange = () => {
+    setYesChecked(true);
+    setNoChecked(false);
+  };
+
+  const handleNoChange = () => {
+    setYesChecked(false);
+    setNoChecked(true);
+  };
+
+  let family;
+  if (yesChecked === true ) {
+    family = 'yes';
+  } else if (noChecked === true ) {
+    family = 'no';
+  }
+
+  // religion
+
+  const [religion, setReligion] = useState('wybierz');
+  const [isOpenReligion, setIsOpenReligion] = useState(false);
+
+
+  const toggleMenuReligion = () => {
+    if (isOpenCompany === false) {
+      setIsOpenReligion(!isOpenReligion);
+    }
+  };
+
+  const handleOptionClickReligion = (option) => {
+    setReligion(option);
+    setIsOpenReligion(false);
+  };
+
+  // company
+
+  const [isOpenCompany, setIsOpenCompany] = useState(false);
+  const [selectedOptionCompany, setSelectedOptionCompany] = useState('');
+  const [inputValueCompany, setInputValueCompany] = useState('');
+  let company;
+
+  const toggleMenuCompany = () => {
+    if (isOpenReligion === false) {
+      setIsOpenCompany(!isOpenCompany);
+    }
+  };
+
+  const handleOptionClickCompany = (option) => {
+    setSelectedOptionCompany(option);
+    setIsOpenCompany(false);
+  };
+
+  const handleOptionClickCompanyOther = (option) => {
+    setSelectedOptionCompany(option);
+  };
+
+  const handleInputChangeCompany = (event) => {
+    setInputValueCompany(event.target.value);
+  };
+
+  const closeCompanies = () => {
+    setIsOpenCompany(false);
+  }
+
+  company = selectedOptionCompany;
+  if (company === 'Inna') {
+    company = inputValueCompany;
+  }
+
+  // otherInfo
+
+  const [otherInfo, setotherInfo] = useState('');
+
+  const handleOtherInfoInputChange = (event) => {
+    const value = event.target.value;
+    setotherInfo(value);
+  };
+
+  //
+
+  console.log(
+    '++++++++++ ZMIENNE ++++++++++',
+    'name: ', name,
+    'surname: ', surname,
+    'weight: ', weight,
+    'family: ', family,
+    'religion: ', religion,
+    'company: ', company,
+    'otherInfo: ', otherInfo,
+    ' '
+  )
+
 return (
-  <div className={props.windowVisibility ? 'your_className': null}>
-  <button className={styles.closeWindowButton} onClick={closeWindow} >X</button>
-    
+  <div className={styles.window}>
+    <div className={styles.innerWindow}>
+      <div>
+        <div>
+          <label><strong>{props.event} {props.dayOfTheWeek} {props.fixedDate}</strong></label>
+        </div>
+        
+        <label>
+          Imię :&nbsp;
+          <input
+            type="text"
+            value={name}
+            onChange={handleNameInputChange}
+            placeholder="Podaj imię"
+          />
+        </label>
+
+        <label>
+          Nazwisko :&nbsp;
+          <input
+            type="text"
+            value={surname}
+            onChange={handleSurnameInputChange}
+            placeholder="Podaj nazwisko"
+          />
+        </label>
+      
+        <label>
+          Waga :&nbsp;
+          <input
+            type="number"
+            value={weight}
+            onChange={handleWeightInputChange}
+            placeholder="Podaj wagę w kilogramach"
+          />
+          &nbsp;kg
+        </label>
+
+        <div>
+          <label>
+            Udział rodziny :&nbsp;
+            <label>
+              <input type="checkbox" checked={yesChecked} onChange={handleYesChange} />
+              Tak
+            </label>
+            <label>
+              <input type="checkbox" checked={noChecked} onChange={handleNoChange} />
+              Nie
+            </label>
+          </label>
+        </div>
+
+        <div>
+          <label>Wyznanie :&nbsp;</label>
+          <button onClick={toggleMenuReligion}>{religion}</button>
+          {isOpenReligion && (
+            <ul className={styles.x}>
+              <li onClick={() => handleOptionClickReligion('chrześcijaństwo')}><button>{religion === 'chrześcijaństwo' ? <strong>chrześcijaństwo</strong> : 'chrześcijaństwo'}</button></li>
+              <li onClick={() => handleOptionClickReligion('islam')}><button>{religion === 'islam' ? <strong>islam</strong> : 'islam'}</button></li>
+              <li onClick={() => handleOptionClickReligion('hinduizm')}><button>{religion === 'hinduizm' ? <strong>hinduizm</strong> : 'hinduizm'}</button></li>
+              <li onClick={() => handleOptionClickReligion('byddyzm')}><button>{religion === 'byddyzm' ? <strong>byddyzm</strong> : 'byddyzm'}</button></li>
+              <li onClick={() => handleOptionClickReligion('sikhizm')}><button>{religion === 'sikhizm' ? <strong>sikhizm</strong> : 'sikhizm'}</button></li>
+              <li onClick={() => handleOptionClickReligion('judaizm')}><button>{religion === 'judaizm' ? <strong>judaizm</strong> : 'judaizm'}</button></li>
+              <li onClick={() => handleOptionClickReligion('bahaizm')}><button>{religion === 'bahaizm' ? <strong>bahaizm</strong> : 'bahaizm'}</button></li>
+              <li onClick={() => handleOptionClickReligion('bezwyznaniowi')}><button>{religion === 'bezwyznaniowi' ? <strong>bezwyznaniowi</strong> : 'bezwyznaniowi'}</button></li>
+            </ul>
+          )}
+        </div>
+
+        <div>
+          <button onClick={toggleMenuCompany}>{company}</button>
+          {isOpenCompany && (
+            <ul className={styles.x}>
+              <li onClick={() => handleOptionClickCompany('Firma 1')}><button>{selectedOptionCompany === 'Firma 1' ? <strong>Firma 1</strong> : 'Firma 1'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 2')}><button>{selectedOptionCompany === 'Firma 2' ? <strong>Firma 2</strong> : 'Firma 2'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 3')}><button>{selectedOptionCompany === 'Firma 3' ? <strong>Firma 3</strong> : 'Firma 3'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 4')}><button>{selectedOptionCompany === 'Firma 4' ? <strong>Firma 3</strong> : 'Firma 4'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 5')}><button>{selectedOptionCompany === 'Firma 5' ? <strong>Firma 3</strong> : 'Firma 5'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 6')}><button>{selectedOptionCompany === 'Firma 6' ? <strong>Firma 3</strong> : 'Firma 6'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 7')}><button>{selectedOptionCompany === 'Firma 7' ? <strong>Firma 3</strong> : 'Firma 7'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 8')}><button>{selectedOptionCompany === 'Firma 8' ? <strong>Firma 3</strong> : 'Firma 8'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 9')}><button>{selectedOptionCompany === 'Firma 9' ? <strong>Firma 3</strong> : 'Firma 9'}</button></li>
+              <li onClick={() => handleOptionClickCompany('Firma 10')}><button>{selectedOptionCompany === 'Firma 10' ? <strong>Firma 3</strong> : 'Firma 10'}</button></li>
+
+              <li onClick={() => handleOptionClickCompanyOther('Inna')}><button>
+                {selectedOptionCompany === 'Inna' ? <strong>Inna</strong> : 'Inna'}</button>
+                {selectedOptionCompany === 'Inna' && (
+                  <div>
+                    <input type="text" value={inputValueCompany} onChange={handleInputChangeCompany} />
+                    <button onClick={closeCompanies} >zatwierdź</button>
+                  </div>
+                )}
+              </li>
+            </ul>
+          )}
+          {/* {selectedOptionCompany && (
+            <p>You selected: {selectedOptionCompany}</p>
+          )}
+          {selectedOptionCompany === 'Inna' && (
+            <p>You typed: {inputValueCompany}</p>
+          )} */}
+        </div>
+
+      </div>
+
+      <div>
+        <button className={styles.closeWindowButton} onClick={closeWindow} >X</button>
+      </div>
+    </div>
+      
+    <label>Uwagi :&nbsp;</label>
+    <textarea
+      className={styles.textarea}
+      value={otherInfo}
+      onChange={handleOtherInfoInputChange}
+    />
+
   </div>
   )
 }
--- src/components/CalendarCrematorium/CalendarElement/Times.js
+++ src/components/CalendarCrematorium/CalendarElement/Times.js
@@ -1,68 +1,69 @@
 import React from 'react'
 import {useState} from 'react';
-import moment from 'moment';
 import styles from './Times.module.scss';
 import Box from './Box/Box';
 
-const time = ['01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00','24:00']
+const times = ['01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00','24:00']
 
 function Times(props) {
 
-  const [event, setEvent] = useState('01:00');
+  const [event, setEvent] = useState('');
   const [info, setInfo] = useState(false);
 
-
-
-  function displayInfo(e) {
-    setInfo(true);
-    setEvent(e.target.innerText);
+  function setTime(e) {
+    if (windowVisibility === false) {
+      setInfo(true);
+      setEvent(e.target.innerText);
+    }
   }
 
-  console.log('====== Times ======');
-  console.log('props', props.fixedDate, '|||||', props);
-  console.log('XDD', event, '|', info);
-  console.log(' ');
+  // console.log('====== Times ======');
+  // console.log('props', props.fixedDate, '|||||', props);
+  // console.log('XDD', event, '|', info);
+  // console.log(' ');
 
   const [windowVisibility, setWindowVisibility] = useState(false);
 
   const openWindow = () => {
-    // setWindowVisibility(!windowVisibility);
     if (windowVisibility === false) {
       setWindowVisibility(true);
-      console.log(windowVisibility);
     }
   };
 
+  // console.log(windowVisibility, event);
+  
 return (
   <>
     <p>
       <span>Wybrany dzień : </span>{event}
     </p>
  
-    <div className={styles.window}>
-      <Box windowVisibility={windowVisibility} setWindowVisibility={setWindowVisibility} />
-    </div>
+    {windowVisibility && (
+      <div className={styles.window}>
+        <Box windowVisibility={windowVisibility} 
+          setWindowVisibility={setWindowVisibility} 
+          setEvent={setEvent} 
+          event={event} 
+          date={props.date} 
+          fixedDate={props.fixedDate} 
+          dayOfTheWeek={props.dayOfTheWeek} 
+          day={props.day} 
+          month={props.month} 
+          year={props.year}
+        />
+      </div>
+    )}
 
     <div className="times">
-      {time.map(times => {
+      {times.map(time => {
         return (
           <div className={styles.timeBox}>
-            <span>{times} {props.dayOfTheWeek} {props.fixedDate}</span>
+            <span>{time} {props.dayOfTheWeek} {props.fixedDate}</span>
 
             <button onClick={(e)=> {
-              displayInfo(e);
+              setTime(e);
               openWindow();
-            }}> {times} </button>
-
-            {/* <button className={isActive ? 'your_className': null} onClick={toggleClass}>
-              korpo szczur krasny
-            </button> */}
-
-            {/* <form>
-              <label className={styles.label}>
-                <input className={styles.input} mbsc-input data-label="First name" type="text" placeholder="First Name" />
-              </label>
-            </form> */}
+            }}> {time} </button>
           </div>
       )
     })}
